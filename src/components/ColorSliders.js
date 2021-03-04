2006ffeb68116cfcc6f8c9eb9d4f3776b10432dc
Your Name <you@example.com>
2021-03-04 01:10:29 -0800

    shvhs

--- src/components/ColorSliders.js
+++ src/components/ColorSliders.js
@@ -16,7 +16,7 @@ function InputRange({ value, min, max }) {
 }
 export default InputRange;*/
 
-
+/*
 import { useState } from "react";
 
 function ColorSliders() {
@@ -25,7 +25,7 @@ function ColorSliders() {
     const [b, setB] = useState(0);
     return (
         <div className="ColorSliders"  style={{ backgroundColor: rgb(${r}, ${g}, ${b}) }}>
-            <div>
+            <div >
                 <input type="range" value={r} min="0" max="255" onInput={({target}) => setR(target.value)} />
             </div>
             <div>
@@ -37,4 +37,37 @@ function ColorSliders() {
         </div>
     );
 }
+export default ColorSliders;*/
+
+import { useState } from "react";
+
+function ColorSliders({ valueA, valueG, valueB }) {
+    const [number1, setNumber1] = useState(valueA ?? "");
+    const [number2, setNumber2] = useState(valueG ?? "");
+    const [number3, setNumber3] = useState(valueB ?? "");
+    return (
+        <div className="ColorSliders" style = {{backgroundColor: `rgb(${number1}, ${number2}, ${number3})`, padding: 10 + "px"}}>
+            <input
+                type="range"
+                min = {0}
+                max = {255}
+                value = {number1}
+                onInput={(event) => setNumber1(event.target.value)} />
+            <br></br>
+            <input
+                type="range"
+                min = {0}
+                max = {255}
+                value = {number2}
+                onInput={(event) => setNumber2(event.target.value)} />
+                <br></br>
+            <input
+                type="range"
+                min = {0}
+                max = {255}
+                value = {number3}
+                onInput={(event) => setNumber3(event.target.value)} />
+        </div>
+    );
+};
 export default ColorSliders;
