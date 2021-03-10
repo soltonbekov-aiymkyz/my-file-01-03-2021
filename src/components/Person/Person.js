8fbe417666c7157ee54ec6bc39786c12ba35f695
Your Name <you@example.com>
2021-03-10 00:38:21 -0800

    df

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,22 +1,3 @@
-// const Person = ({name,age,children}) => {
-//     return ( <div style={
-//         {
-//          boxShadow:"0 0 10px , black",
-//          borderRadius:"10px",
-//          padding:"15px",
-//          paddingTop:"5px",
-//          margin:"10px",
-//          display:"inline-block",
-//         }
-//     }>
-//         <h2>{ name }</h2>
-//         <h4> {age } years old</h4>
-//         <h5>{ children }</h5>
-//     </div> );
-// }
- 
-// export default Person;
-
 import classes from "./Person.module.css";
 const Person = ({ name, age, children }) => {
     return (<div className={classes.Person}>
